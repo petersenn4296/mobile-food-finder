c43b850902bb3cce70aafc758b0723f2059acb0e
Peter S <petersenn4296@gmail.com>
2018-06-19 15:29:44 -0600

    create owner and truck fully done with redirection complete

--- public/js/createtruck.js
+++ public/js/createtruck.js
@@ -1,22 +1,13 @@
 $(document).ready(function() {
-  // $(function(){
-  //
-  //   $("#dropdownMenuButton").click(function(){
-  //
-  //     $(".btn:first-child").text($(this).text());
-  //     $(".btn:first-child").val($(this).text());
-  //
-  //   });
-  //
-  // });
 
 // Creating a truck
 $('#truckcreate').submit((e)=> {
-  console.log('i am in here');
   e.preventDefault()
   let data = $('#truckcreate').serialize();
+  console.log('i am in here', data);
   $.post('/create_truck', data).done(function(result) {
     $('.result').html(result)
+
     });
   })
 
--- public/js/register.js
+++ public/js/register.js
@@ -5,7 +5,8 @@ $('#register').submit(function(e) {
   let data = $('#register').serialize();
   // console.log('this is the data', data);
   $.post('/owner_signup', data).done(function(result) {
+    window.location.href = "create_truck.html"
     console.log(result);
-    })
+  })
   })
 })
