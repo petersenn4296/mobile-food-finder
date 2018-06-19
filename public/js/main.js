f04035fd751995e090736d1c62763fd854fe40b1
Peter S <petersenn4296@gmail.com>
2018-06-19 09:48:04 -0600

    added new refactored front end

---
+++ public/js/main.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+
+//Owners sign on handlers
+$('#login').click(function() {
+  console.log('clicked login');
+    //code to submit form to verify owner in database AJAX request
+  })
+
+$('#register').click(function() {
+  console.log('clicked register');
+    //code to submit form to database to make owners AJAX request
+  })
+
+
+
+// Creating a truck
+$('#createtruck').click(function() {
+  console.log('clicked create truck');
+    //code to submit form to database to create a truck AJAX request
+  })
+})
