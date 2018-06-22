c1360496d6acd543f46ff08925d5dbc60e1ca6a6
Peter S <petersenn4296@gmail.com>
2018-06-22 09:30:52 -0600

    got the dropdown to popluate properly

--- public/js/createtruck.js
+++ public/js/createtruck.js
@@ -1,17 +1,10 @@
 $(document).ready(function() {
-// $('.dropdown-item').click(function(e)  {
-//   e.preventDefault()
-//   console.log('clicked this');
-//   $('#cuisinechoice').text('mom')
-// })
 // Creating a truck
 $('#truckcreate').submit((e)=> {
   e.preventDefault()
   let data = $('#truckcreate').serialize();
   $.post('/create_truck', data).done(function(result) {
     window.location.href = "/owner_home.html"
-
     });
   })
-
 })
